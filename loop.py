06c66ed756e348dbd4d7399992313a4857f41b83
LEONARDO SOUZA LEANDRO <128157527+LEONARDO-LEANDRO@users.noreply.github.com>
2024-03-06 09:51:25 -0300

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,22 @@
+nomes = ['Leo', 'Rafa','Miguel','João','José']
+for nome in nomes:
+        print(nome)
+
+        print("__________")
+
+
+soma = 0
+for i in range(1,11):
+        soma = soma +1
+        print(soma)
+
+        print("-------")
+
+
+n = 5
+fatorial = 1
+for i in range(1, n+1):
+    fatorial = fatorial * i
+print(f"O fatorial de {n} é {fatorial}")
+
+        
